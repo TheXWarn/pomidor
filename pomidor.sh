3d097534f57b5d68d17ae2c9c5a6476b05256730
Nusret <89344065+nusrte@users.noreply.github.com>
2022-04-14 14:30:12 +0400

    Create pomidor.sh

---
+++ pomidor.sh
@@ -0,0 +1,28 @@
+MESAJ="YarasaUserBot'un avtomatik qurulumu."
+MESAJ+="\nTelegram: @YarasaBots"
+pkg update -y
+apt updete 
+apt upgrade
+clear
+echo -e $MESAJ
+echo "Python Yüklənir"
+pkg install python -y
+clear
+echo -e $MESAJ
+echo "Git Yüklənir"
+pkg install git -y
+clear
+echo -e $MESAJ
+echo "TeleThon Yüklənir"
+pip install telethon
+echo "Repo klonlanır..."
+git clone https://github.com/xtq067/xiyar
+clear
+echo -e $MESAJ
+cd xiyar
+clear
+echo "Bəzi Alt Yapi Dosyaları Yüklənir"
+echo -e $MESAJ
+pip install wheel
+pip install -r requirements.txt
+python -m yarasa 
